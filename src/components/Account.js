1aa174b2c5dec1dd17355cab038a5e0497584854
rdronio <rdenver0315@gmail.com>
2021-03-02 16:38:05 +0800

    updated account component

--- src/components/Account.js
+++ src/components/Account.js
@@ -2,6 +2,86 @@ const Account = () => {
   return (
     <div className="account container mx-auto p-6">
       <h2>Account</h2>
+
+      <div className="hidden sm:block" aria-hidden="true">
+        <div className="py-5">
+          <div className="border-t border-gray-200"></div>
+        </div>
+      </div>
+
+      <div className="mt-10 sm:mt-0">
+        <div className="md:grid md:grid-cols-3 md:gap-6">
+          <div className="md:col-span-1">
+            <div className="px-4 sm:px-0">
+              <h3>Account Information</h3>
+              <p className="mt-1 text-sm text-gray-600">
+                Please use a valid details.
+              </p>
+            </div>
+          </div>
+          <div className="mt-5 md:mt-0 md:col-span-2">
+            <form action="#" method="POST">
+              <div className="shadow overflow-hidden sm:rounded-md">
+                <div className="px-4 py-5 bg-white sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="separator">
+                      <label for="first_name">First name</label>
+                      <input
+                        type="text"
+                        name="first_name"
+                        id="first_name"
+                        autocomplete="given-name"
+                      />
+                    </div>
+
+                    <div className="separator">
+                      <label for="last_name">Last name</label>
+                      <input
+                        type="text"
+                        name="last_name"
+                        id="last_name"
+                        autocomplete="family-name"
+                      />
+                    </div>
+
+                    <div className="separator">
+                      <label for="email_address">Email address</label>
+                      <input
+                        type="text"
+                        name="email_address"
+                        id="email_address"
+                        autocomplete="email"
+                      />
+                    </div>
+
+                    <div className="separator">
+                      <label for="position">Position</label>
+                      <select
+                        id="position"
+                        name="position"
+                        autocomplete="position"
+                      >
+                        <option>Administrator</option>
+                        <option>Approver</option>
+                        <option>Validator</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="button-container">
+                  <button type="submit">Save</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="hidden sm:block" aria-hidden="true">
+        <div className="py-5">
+          <div className="border-t border-gray-200"></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,94 @@
+import { ReactComponent as Logo } from '../logo.svg';
 const Login = () => {
   return (
-    <div className="container mx-auto p-6">
-      <h2>login</h2>
+    //     <!--
+    //   This example requires Tailwind CSS v2.0+
+
+    //   This example requires some changes to your config:
+
+    //   ```
+    //   // tailwind.config.js
+    //   module.exports = {
+    //     // ...
+    //     plugins: [
+    //       // ...
+    //       require('@tailwindcss/htmlForms'),
+    //     ]
+    //   }
+    //   ```
+    // -->
+
+    <div
+      id="login"
+      className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8"
+    >
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <img src={Logo} alt="" srcset="" />
+          <h2>Sign in to your account</h2>
+        </div>
+        <htmlForm className="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm ">
+            <div className="mt-2">
+              <label htmlFor="email-address">Email address</label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autocomplete="email"
+              />
+            </div>
+            <div className="mt-2">
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input id="remember_me" name="remember_me" type="checkbox" />
+              <label htmlFor="remember_me" className="remember-me">
+                Remember me
+              </label>
+            </div>
+
+            <div className="text-sm">
+              <a href="#" className="forgot-password">
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <button type="submit">
+              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                {/* <!-- Heroicon name: solid/lock-closed --> */}
+                <svg
+                  className="h-5 w-5 text-blue-500 group-hover:text-indigo-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+              Sign in
+            </button>
+          </div>
+        </htmlForm>
+      </div>
     </div>
   );
 };
