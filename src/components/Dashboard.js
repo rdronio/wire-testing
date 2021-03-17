491c92969d8e7f93dcd4abb6c433decab9a13ed9
rdronio <rdenver0315@gmail.com>
2021-03-17 16:53:15 +0800

    updated favicon and logo

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -21,30 +21,51 @@ const Dashboard = () => {
         </div>
       </div>
       <div id="body" className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        {/* <!-- Replace with your content --> */}
-        <div className="px-4 py-6 sm:px-0">
-          <div className="card">
-            <h3>Date Today </h3>
-            <p>{date}</p>
+        {/* Card */}
+        <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Information
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              Following are valid details.
+            </p>
           </div>
-          <div className="card">
-            <h3>Total Results </h3>
-            <p>{total_results}</p>
-          </div>
-          <div className="card">
-            <h3>Approved </h3>
-            <p>{total_approved}</p>
-          </div>
-          <div className="card">
-            <h3>More Testing Required</h3>
-            <p>{total_retest}</p>
-          </div>
-          <div className="card">
-            <h3>Failed </h3>
-            <p>{total_failed}</p>
+          <div className="border-t border-gray-200">
+            <dl>
+              <div className="view-info-separator">
+                <dt className="text-sm font-medium text-gray-500">
+                  Total Results
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  1300
+                </dd>
+              </div>
+
+              <div className="view-info-separator">
+                <dt className="text-sm font-medium text-gray-500">Approved</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  500
+                </dd>
+              </div>
+
+              <div className="view-info-separator">
+                <dt className="text-sm font-medium text-gray-500">
+                  More Testing Required
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  500
+                </dd>
+              </div>
+              <div className="view-info-separator">
+                <dt className="text-sm font-medium text-gray-500">Failed</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  500
+                </dd>
+              </div>
+            </dl>
           </div>
         </div>
-        {/* <!-- /End replace --> */}
       </div>
     </section>
   );
