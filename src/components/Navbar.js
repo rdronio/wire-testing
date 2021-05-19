7cb9ea466173af39bae743c4476f37893c0a9ead
rdronio <rdenver0315@gmail.com>
2021-05-19 16:10:30 +0800

    Updated navbar user menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,6 +12,9 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   const handleToggleMenu = () => setToggleMenu(!toggleMenu);
 
+  const [toggleUserMenu, setToggleUserMenu] = useState(false);
+  const handleToggleUserMenu = () => setToggleUserMenu(!toggleUserMenu);
+
   const [navbarLinks, setNavbarLinks] = useState([
     {
       label: 'Dashboard',
@@ -112,12 +115,15 @@ const Navbar = () => {
               <div className="ml-3 relative">
                 <div>
                   <button
-                    className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                    className=" px-1 max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                     id="user-menu"
                     aria-haspopup="true"
+                    onClick={handleToggleUserMenu}
                   >
                     <span className="sr-only">Open user menu</span>
-                    {/* <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""> */}
+                    <span>
+                      <i className="fas fa-user-circle text-white text-3xl "></i>
+                    </span>
                   </button>
                 </div>
                 {/* <!--
@@ -129,33 +135,28 @@ const Navbar = () => {
                       From: "transform opacity-100 scale-100"
                       To: "transform opacity-0 scale-95"
                   --> */}
-                <div
-                  className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="user-menu"
-                >
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
+                <div className={toggleUserMenu ? '' : 'md:hidden lg:hidden'}>
+                  <div
+                    className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="user-menu"
+                    id="user-menu"
                   >
-                    Your Profile
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
-                  >
-                    Settings
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
-                  >
-                    Sign out
-                  </a>
+                    {navbarLinks
+                      .filter((item) => item.type === 'user-menu')
+                      .map((filteredItem, index) => {
+                        return (
+                          <Link
+                            to={filteredItem.path}
+                            key={index}
+                            className="user-menu"
+                          >
+                            <span>{filteredItem.label}</span>
+                          </Link>
+                        );
+                      })}
+                  </div>
                 </div>
               </div>
             </div>
